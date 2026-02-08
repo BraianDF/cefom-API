2ba3eb94a68cc93f65ab30dd29f1463b448136f8
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-02-08 15:27:44 -0300

    feat: adiciona classe TurmaController

---
+++ src/main/java/com/projeto/cefom/novos/controller/TurmaController.java
@@ -0,0 +1,80 @@
+package com.projeto.cefom.novos.controller;
+
+import com.projeto.cefom.dto.request.EntrevistaRequestDTO;
+import com.projeto.cefom.dto.request.VinculoEntrevistaMatriculaRequestDTO;
+import com.projeto.cefom.dto.response.EntrevistaListarResponseDTO;
+import com.projeto.cefom.dto.response.EntrevistaResponseDTO;
+import com.projeto.cefom.dto.response.EscolaSelectResponseDTO;
+import com.projeto.cefom.novos.dto.request.AlunoParticipacaoRequestDTO;
+import com.projeto.cefom.novos.dto.request.TurmaRequestDTO;
+import com.projeto.cefom.novos.dto.response.TurmaListarResponseDTO;
+import com.projeto.cefom.novos.dto.response.TurmaResponseDTO;
+import com.projeto.cefom.novos.dto.response.TurmaSelectResponseDTO;
+import com.projeto.cefom.novos.service.TurmaService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/turmas")
+public class TurmaController {
+    private final TurmaService turmaService;
+
+    public TurmaController(TurmaService turmaService) {
+        this.turmaService = turmaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TurmaResponseDTO> criar(@Valid @RequestBody TurmaRequestDTO dto) {
+        TurmaResponseDTO response = turmaService.criar(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{idTurma}")
+    public ResponseEntity<TurmaResponseDTO> atualizar(@PathVariable Integer idTurma, @Valid @RequestBody TurmaRequestDTO dto) {
+        TurmaResponseDTO response = turmaService.atualizar(idTurma, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TurmaListarResponseDTO>> listar(@PageableDefault(page = 0, size = 10) Pageable pageable, @RequestParam(defaultValue = "") String nome) {
+        Page<TurmaListarResponseDTO> response = turmaService.listar(pageable, nome);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/select")
+    public ResponseEntity<List<TurmaSelectResponseDTO>> listarSelect() {
+        List<TurmaSelectResponseDTO> response = turmaService.listarSelect();
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @GetMapping("/{idTurma}")
+    public ResponseEntity<TurmaResponseDTO> buscarPorId(@PathVariable Integer idTurma) {
+        TurmaResponseDTO response = turmaService.buscarPorId(idTurma);
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @DeleteMapping("/{idTurma}")
+    public  ResponseEntity<Void> excluirPorId(@PathVariable Integer idTurma) {
+        turmaService.excluirPorId(idTurma);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @PostMapping("/{idTurma}/adolescentes")
+    public ResponseEntity<TurmaResponseDTO> adicionarAdolescente(@PathVariable Integer idTurma, @Valid @RequestBody AlunoParticipacaoRequestDTO dto) {
+        TurmaResponseDTO response = turmaService.adicionarAdolescente(idTurma, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PutMapping("/{idTurma}/adolescentes")
+    public ResponseEntity<TurmaResponseDTO> removerAdolescente(@PathVariable Integer idTurma, @Valid @RequestBody AlunoParticipacaoRequestDTO dto) {
+        TurmaResponseDTO response = turmaService.removerAdolescente(idTurma, dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
