a3133c05541f5305f9baa324b3b59f3023d2b59c
BraianDF <geraldo.bryan.vbnb@gmail.com>
2026-02-20 15:23:55 -0300

    feat: remove vínculos de endereços ao excluir território

--- src/main/java/com/projeto/cefom/service/TerritorioService.java
+++ src/main/java/com/projeto/cefom/service/TerritorioService.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -129,6 +130,14 @@ public class TerritorioService {
     @Transactional
     public void excluirPorId(Integer idTerritorio) {
         Territorio territorio = buscarTerritorio(idTerritorio);
+
+        Set<String> bairros = new HashSet<>(territorio.getBairros());
+
+        bairros.stream().forEach(b -> {
+            territorio.removerBairro(b);
+            territorioAplicacaoService.removerTerritorioEnderecos(b);
+        });
+
         territorioRepository.deleteById(idTerritorio);
     }
 
